09bb904fea1fac598089e3ad01e71a7230c51ae0
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2025-10-24 02:09:19 +0300

    add card component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,73 +1,79 @@
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/Card";
 import Link from "next/link";
 
 function LoginPage() {
   return (
-    <div className="w-full max-w-xl shadow-xl border border-border rounded-xl">
-      {/* Login Header */}
-      <header className="text-center space-y-1 p-6">
+    <Card>
+      {/* Header */}
+      <CardHeader>
         <h1 className="text-2xl font-bold text-primary">
           Login into your account
         </h1>
         <p className="text-sm text-muted">
           Please log in or sign up to continue using our app.
         </p>
-      </header>
+      </CardHeader>
 
       {/* Form */}
-      <form className="flex flex-col gap-5 px-6 py-8 border-y border-border">
-        {/* Email */}
-        <div>
-          <label className="block text-sm font-medium text-text-heading mb-1">
-            Email
-          </label>
-          <input
-            type="email"
-            className="input w-full"
-            placeholder="Enter your email"
-          />
-        </div>
-
-        {/* Password */}
-        <div>
-          <label className="block text-sm font-medium text-text-heading mb-1">
-            Password
-          </label>
-          <input
-            type="password"
-            className="input w-full"
-            placeholder="Enter your password"
-          />
-        </div>
-
-        {/* Remember Me + Forgot Password */}
-        <div className="flex items-center justify-between text-sm">
-          <label className="flex items-center gap-2 text-muted-foreground cursor-pointer">
+      <CardContent>
+        <form className="flex flex-col gap-5 ">
+          {/* Email */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              Email
+            </label>
             <input
-              type="checkbox"
-              className="w-4 h-4 accent-primary cursor-pointer"
-              id="remember"
+              type="email"
+              className="input w-full"
+              placeholder="Enter your email"
             />
-            Remember me
-          </label>
+          </div>
 
-          <Link href="/forgotPassword" className="text-primary hover:underline">
-            Forgot Password?
-          </Link>
-        </div>
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Enter your password"
+            />
+          </div>
 
-        <button type="submit" className="main-btn w-full">
-          Login
-        </button>
-      </form>
+          {/* Remember Me + Forgot Password */}
+          <div className="flex items-center justify-between text-sm">
+            <label className="flex items-center gap-2 text-muted-foreground cursor-pointer">
+              <input
+                type="checkbox"
+                className="w-4 h-4 accent-primary cursor-pointer"
+                id="remember"
+              />
+              Remember me
+            </label>
 
-      {/* Login Footer */}
-      <p className="text-center py-3 text-sm text-muted">
+            <Link
+              href="/forgotPassword"
+              className="text-primary hover:underline"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+
+          <button type="submit" className="main-btn w-full">
+            Login
+          </button>
+        </form>
+      </CardContent>
+
+      {/* Footer */}
+      <CardFooter>
         Don&apos;t have an account ?{" "}
         <Link href="/register" className="text-primary underline">
           Register
         </Link>
-      </p>
-    </div>
+      </CardFooter>
+    </Card>
   );
 }
 
