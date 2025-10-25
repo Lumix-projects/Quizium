c7135b0a6ac23532b0a4a23d801d6a0b35d6efb8
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2025-10-25 14:07:32 +0300

    add register ui , dynamic Auth Layout with conditional images

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,125 @@
-import React from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/Card";
+import Link from "next/link";
 
-function page() {
-  return <div>register</div>;
+function RegisterPage() {
+  return (
+    <Card>
+      {/* Header */}
+      <CardHeader>
+        <h1 className="text-xl lg:text-2xl font-bold text-primary">
+          Create your account
+        </h1>
+        <p className="text-xs lg:text-sm text-muted">
+          Please sign up to get started and enjoy all our quizzes.
+        </p>
+      </CardHeader>
+
+      {/* Form */}
+      <CardContent>
+        <form className="flex flex-col gap-3 lg:gap-5">
+          {/* First + Last Name */}
+          <div className="flex flex-col sm:flex-row gap-5">
+            <div className="flex-1">
+              <label className="block text-sm font-medium text-text-heading mb-1">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="First name"
+              />
+            </div>
+
+            <div className="flex-1">
+              <label
+                htmlFor="lastName"
+                className="block text-sm font-medium text-text-heading mb-1"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Last name"
+              />
+            </div>
+          </div>
+
+          {/* user name */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              User name
+            </label>
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Enter your user name"
+            />
+          </div>
+
+          {/* Email */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          {/* Phone number */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              Phone Number
+            </label>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Enter your phone number"
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          {/* Confirm Password */}
+          <div>
+            <label className="block text-sm font-medium text-text-heading mb-1">
+              Confirm password
+            </label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Confirm your password"
+            />
+          </div>
+
+          <button type="submit" className="main-btn w-full">
+            Login
+          </button>
+        </form>
+      </CardContent>
+
+      {/* Footer */}
+      <CardFooter>
+        Already have an account ?{" "}
+        <Link href="/login" className="text-primary underline">
+          Login
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 }
 
-export default page;
+export default RegisterPage;
