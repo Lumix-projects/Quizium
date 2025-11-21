d77b9c4cce219979bdbf222819a51089dda88ccd
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2025-11-21 20:12:51 +0200

    refactor useLogin hook

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -3,9 +3,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { loginUser } from "@/services/auth"; // ← غيّرها لاسم API اللوجين عندك
+import { loginUser } from "@/services/auth";
 import { LoginData } from "@/types/auth";
-import { loginSchema, LoginSchema } from "@/schemas/loginschema";
+import { loginSchema, LoginSchema } from "@/schemas/loginSchema";
 import { useRouter } from "next/navigation";
 
 export function useLogin() {
@@ -26,20 +26,23 @@ export function useLogin() {
   });
 
   async function login(values: LoginData) {
-    try {
-      const response = await loginUser(values);
+    // Call backend registration service
+    const result = await loginUser(values);
 
-      if (response && response.user) {
-        router.push("/");
-        reset();
-      } else {
-        toast.error("Invalid email or password");
-      }
-    } catch (error: any) {
-      toast.error(error.message || "Something went wrong");
-      console.error(error);
+    // Show error and abort if registration failed
+    if (result.error) {
+      toast.error(result.error);
+      return;
     }
-  }
 
+    // Show success toast
+    toast.success("login successful!");
+
+    // Redirect to home page after successful registration
+    router.push("/");
+
+    // Reset form after registration
+    reset();
+  }
   return { register, handleSubmit, errors, login, isSubmitting };
 }
