4532acaffe3a77822dfc5f09ee2b5ef68d6260bc
ahmed-abdelmottal3 <abdelmottale3@gmail.com>
2025-11-24 20:08:46 +0200

    fix next error and adjust jotal quizzes indecator in home page

--- src/app/(pages)/user/page.tsx
+++ src/app/(pages)/user/page.tsx
@@ -10,11 +10,12 @@ import {
 } from "react-icons/fi";
 import { useUser } from "@/hooks/useUser";
 import { useScores } from "@/hooks/useScores";
+import { useExams } from "@/hooks/useSubject";
 
 export default function HomePage() {
   const { user, loading: userLoading } = useUser();
   const { scores, loading: scoresLoading } = useScores();
-
+  const { exams } = useExams();
   const loading = userLoading || scoresLoading;
 
   if (loading) {
@@ -24,15 +25,6 @@ export default function HomePage() {
       </div>
     );
   }
-
-  // Calculate Stats
-  const totalQuizzes = scores.length;
-  const averageScore =
-    totalQuizzes > 0
-      ? Math.round(
-          scores.reduce((acc, curr) => acc + curr.percentage, 0) / totalQuizzes
-        )
-      : 0;
 
   const timeSpent = "4h 30m";
   const activeStreak = "3 Days";
@@ -55,13 +47,13 @@ export default function HomePage() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <StatsCard
           title="Total Quizzes"
-          value={totalQuizzes}
+          value={exams.length}
           icon={FiCheckCircle}
           color="blue"
         />
         <StatsCard
           title="Average Score"
-          value={`${averageScore}%`}
+          value={`0%`}
           icon={FiTrendingUp}
           color="main"
         />
@@ -100,11 +92,10 @@ export default function HomePage() {
                 >
                   <div className="flex items-center gap-4">
                     <div
-                      className={`w-10 h-10 rounded-full flex items-center justify-center ${
-                        isPassed
-                          ? "bg-success/10 text-success"
-                          : "bg-error/10 text-error"
-                      }`}
+                      className={`w-10 h-10 rounded-full flex items-center justify-center ${isPassed
+                        ? "bg-success/10 text-success"
+                        : "bg-error/10 text-error"
+                        }`}
                     >
                       {isPassed ? (
                         <FiCheckCircle className="text-lg" />
@@ -127,9 +118,8 @@ export default function HomePage() {
                       {score.percentage}%
                     </span>
                     <span
-                      className={`text-xs font-medium ${
-                        isPassed ? "text-success" : "text-error"
-                      }`}
+                      className={`text-xs font-medium ${isPassed ? "text-success" : "text-error"
+                        }`}
                     >
                       {isPassed ? "Passed" : "Failed"}
                     </span>
